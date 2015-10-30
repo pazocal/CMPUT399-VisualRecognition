eb61acb0a0e73a5658b6a80a8c80f29a0f29c449
jiyange <labixiaoyangji@qq.com>
2015-10-29 22:28:05 -0600

    Start construcIMDB

---
+++ SVM/simple_svm2.m
@@ -0,0 +1,90 @@
+% Illustrates a simple SV classifer
+close all;
+clear all;
+separable=0;
+lambda=1;
+
+if separable==1,
+    % separable case
+    n=25;
+    x1 = 100*rand(n,1);
+    x2 = 100*rand(n,1);
+    % training labels
+    figure(1),hold on;
+    y=zeros(n,1);
+    for i=1:n,
+        if 0.4*x1(i)+30>x2(i),
+            y(i) = 1;
+            plot(x1(i),x2(i),'+');
+        else
+            y(i) = -1;
+            plot(x1(i),x2(i),'r.');
+        end
+    end
+    hold off;
+else
+    % non-separable case
+    load gaussian_mixture_data;
+    n=length(y);
+    figure(1),plot(x1(1:100),x2(1:100),'+');
+    figure(1),hold on, plot(x1(101:200),x2(101:200),'r.'); hold off; drawnow;
+end
+% construxt Gram matrix (Kernel matrix)
+K = zeros(n,n);
+for i=1:n,
+    for j=1:n,
+%         K(i,j) = x1(i)*x1(j) + x2(i)*x2(j); % this a linear kernel, it could be more sophisticated
+        K(i,j) = exp(-((x1(i)-x1(j))^2 + (x2(i)-x2(j))^2)/lambda); % this a linear kernel, it could be more sophisticated
+    end
+end
+C=1; % a parameter, you actually don't need it for the separable case
+alpha = quadprog(diag(y)*K*diag(y),-ones(n,1),eye(n),C*ones(n,1),y',0,zeros(n,1),C*ones(n,1));
+
+% support vectors
+tol=0.0001;
+sv = zeros(length(y),1);
+figure(1),hold on;
+for i=1:length(y),
+    if alpha(i)>tol,
+        sv(i)=1;
+        plot(x1(i),x2(i),'o');
+    end
+end
+hold off;
+
+% computing offset
+b = 0;
+for i=1:length(y),
+    if sv(i),
+        b = b + y(i);
+        for j=1:length(y),
+            if sv(j),
+                b = b - alpha(j)*y(j)*K(i,j);
+            end
+        end
+    end
+end
+b = b/sum(sv);
+
+[A,B] = meshgrid(-1:0.1:3,-0.7:0.1:2.8);
+Z = zeros(size(A));
+for i=1:size(A,1),
+    for j=1:size(A,2),
+        v=0;
+        for k=1:n,
+            if alpha(k)>=eps,
+                v = v + y(k)*alpha(k)*exp(-((x1(k)-A(i,j))^2 + (x2(k)-B(i,j))^2)/lambda);
+            end
+        end
+        v = v + b;
+        Z(i,j) = v;
+    end
+end
+figure,imagesc(Z>=0),axis image;
+% now constructing the hyper plane (in this case a straight line)
+b1 = sum((((sv.*alpha).*y).*x1));
+b2 = sum((((sv.*alpha).*y).*x2));
+
+px = (min(x1):max(x1))';
+py = -(b1/b2)*px - (b/b2);
+figure(1); hold on; plot(px,py,'g'); hold off; axis image;
