d826a03d85c0ac6a928875718f59935e639fb5a5
jiyange <labixiaoyangji@qq.com>
2015-10-29 22:47:10 -0600

    Now constructIMDB.m works with img set

--- Assignment 2/constructIMDB.m
+++ Assignment 2/constructIMDB.m
@@ -31,17 +31,42 @@ save_template='loop_closure_imdb';
 % Here you will be reading images, calling "getFeatures" function and populating
 % imdb.images.name, imdb.images.frames and imdb.images.descrs cell arrays
 
+Image_type = '/*.ppm';
+
+disp('Please wait for a long time!');
+ImageUse = dir([imdb.dir Image_type]);
+for i=1:count
+    imdb.images.name{1,i} = ImageUse(i).name;
+    image = imread([imdb.dir '/' ImageUse(i).name]);
+    [frame, descrs] = getFeatures(image,imdb.featureOpts{:}) ;
+    imdb.images.frames{1,i} = frame;
+    imdb.images.descrs{1,i} = descrs;
+end
+
 % Step 3: Construct a vocabulary of words from the combined features of all the images
 % using K-Means clustering. Call "vl_kmeans" function with combined features and
 % imdb.numWords to assign vocabulary to imdb.vocab
 
+des = cat(2,imdb.images.descrs{:});
+centerWord = vl_kmeans(des,imdb.numWords,'algorithm','ANN');
+imdb.vocab = centerWord;
+
 % Step 4: Construct a KD tree from this vocabulary and assign to imdb.kdtree
+
+imdb.tree = vl_kdtreebuild(centerWord);
 
 % Step 5: Find the words present in each image through NN search of the
 % vocabulary to populate imdb.images.words cell array. Use "vl_kdtreequery" 
 % function for the NN search.
 
+
+for i = 1:count
+    imdb.images.words{i} = vl_kdtreequery(imdb.tree, imdb.vocab,imdb.images.descrs{i});
+end
+
+
 % Step 6: Compute indexes and idf weights by calling "loadIndex" function
+imdb = loadIndex(imdb);
 
 % Step 7: Save imdb in a .mat file
 save(strcat(save_template, '.mat'), 'imdb');
